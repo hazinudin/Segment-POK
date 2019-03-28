e4f5ec90e7afebe62050b3ef1bdb67c688407a2a
hazinudin <hazinudin@hotmail.com>
2019-03-28 11:16:09 +0700

    Add Overlap Detection Logic

--- DetectOverlap.py
+++ DetectOverlap.py
@@ -1,26 +1,38 @@
-from arcpy import da, GetParameterAsText, AddMessage
+from arcpy import da, GetParameterAsText, AddMessage, SelectLayerByAttribute_management
 import pandas as pd
-import numpy as np
 
 EventTable = GetParameterAsText(0)
 RouteId = GetParameterAsText(1)
 FromMeas = GetParameterAsText(2)
 ToMeas = GetParameterAsText(3)
-OutputTable = GetParameterAsText(4)
-Resolution = 0.01
+OverlapOID = GetParameterAsText(4)
 
-np_array = da.FeatureClassToNumPyArray(EventTable, [RouteId, FromMeas, ToMeas, 'SHAPE@M'])
+OID_field = 'OID@'
+np_array = da.FeatureClassToNumPyArray(EventTable, [RouteId, FromMeas, ToMeas, OID_field])
 df = pd.DataFrame(np_array)
+AddMessage(list(df))
 route_list = df[RouteId].unique().tolist()
 
 for route in route_list:
     df_route = df.loc[df[RouteId] == route]
+
+    flipped_i = df_route.loc[df_route[FromMeas] > df_route[ToMeas]].index
+    df_route.loc[flipped_i, [FromMeas, ToMeas]] = df_route.loc[flipped_i, [ToMeas, FromMeas]].values
     df_route.sort_values(by=[FromMeas, ToMeas], inplace=True)
-    df_route.reset_index(drop=True, inplace=True)
 
     for index, row in df_route.iterrows():
-        if index == 0:
-            from_m = row[FromMeas]
-            to_m = row[ToMeas]
-        else:
-            pass
+        overlap_i = df_route.loc[((df_route[FromMeas] < row[FromMeas]) & (df_route[ToMeas] > row[FromMeas])) |
+                                 ((df_route[FromMeas] > row[FromMeas]) & (df_route[ToMeas] < row[ToMeas])) |
+                                 ((df_route[FromMeas] < row[ToMeas]) & (df_route[FromMeas] > row[FromMeas]))].index
+        if len(overlap_i) != 0:
+            OID_list = df_route.loc[overlap_i, OID_field].values.tolist()
+            OID_list = str(OID_list).strip('[]')
+
+            if len(OID_list) > 195:
+                OID_list = None
+
+            with da.UpdateCursor(EventTable, OverlapOID,
+                                 where_clause="OBJECTID IN ({0})".format(row[OID_field])) as cursor:
+                for update_row in cursor:
+                    update_row[0] = OID_list
+                    cursor.updateRow(update_row)
